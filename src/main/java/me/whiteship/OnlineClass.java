06eca4303e4f1346c4852f634707052f39d61835
lby132 <quddbscjswow@naver.com>
2022-12-20 21:09:49 +0900

    stream

---
+++ src/main/java/me/whiteship/OnlineClass.java
@@ -0,0 +1,46 @@
+package me.whiteship;
+
+public class OnlineClass {
+    private int id;
+    private String title;
+    private boolean closed;
+    private Progress progress;
+
+    public OnlineClass(int id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    public Progress getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+}
