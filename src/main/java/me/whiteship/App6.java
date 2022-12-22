c9c08c8f46a53c246c224803a7b28914ff44da22
lby132 <quddbscjswow@naver.com>
2022-12-22 13:43:27 +0900

    Executors.newSingleThreadScheduledExecutor

--- src/main/java/me/whiteship/App6.java
+++ src/main/java/me/whiteship/App6.java
@@ -2,16 +2,14 @@ package me.whiteship;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class App6 {
     public static void main(String[] args) {
         //Executors는 스레드를 만들고 관리하고 작업을 실행한다.
-        final ExecutorService executorService = Executors.newFixedThreadPool(2);//스레드 풀을 두개 생성했기 때문에 두개의 스레드로 번갈아가면서 코드를 실행한다.
-        executorService.submit(getRunnable("Hello"));
-        executorService.submit(getRunnable("Hello1"));
-        executorService.submit(getRunnable("Hello2"));
-        executorService.submit(getRunnable("Hello3"));
-        executorService.submit(getRunnable("Hello4"));
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(getRunnable("Hello"), 3, TimeUnit.SECONDS);    //3초 후에 실행
 
         executorService.shutdown(); //현재 작업중인것들은 다 마치고 끝낸다.
     }
